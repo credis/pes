e279e1a742ac3aa5e251a6acbec42c15ad435611
Claude Huchet <claude.huchet@quinode.fr>
2012-10-11 14:53:02 +0200

    un peu de lisibilité

--- pes/models.py
+++ pes/models.py
@@ -7,3 +7,7 @@ from django.db import models
 # For suggestion purpose
 class Word(models.Model):
     name = models.CharField(_(u'name'), blank=True, null=True, max_length=255, editable=False)
+
+    def __unicode__(self):
+        return unicode(self.name)
+
