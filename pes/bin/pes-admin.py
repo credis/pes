efc484394050693268a76470eaf2c67b0bef4a29
Claude Huchet <claude.huchet@quinode.fr>
2012-10-31 11:19:23 +0100

    typo in pes-admin

--- pes/bin/pes-admin.py
+++ pes/bin/pes-admin.py
@@ -9,7 +9,7 @@ import pes.management
 if __name__ == "__main__":
     # global management._commands
     if management._commands is None:
-        management._commands = dict([(name, 'coop') for name in management.find_commands(pes.management.__path__[0])])
+        management._commands = dict([(name, 'pes') for name in management.find_commands(pes.management.__path__[0])])
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pes.minimal_settings")
 
 
